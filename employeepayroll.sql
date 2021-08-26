98ff3aa6c6ab858586bf89b141c8109d28e3ec02
pavanyalala <pavanyalala4508@gmail.com>
2021-08-26 15:06:09 +0530

    [pavan]:UC3:employeepayroll data  added to columns

--- employeepayroll.sql
+++ employeepayroll.sql
@@ -7,3 +7,13 @@ Name varchar(20),
 Salary Varchar(10),
 StartDate varchar(10) 
 )
+
+insert into employee_payroll (Name, Salary, StartDate) values ('Bunny', '10000', '25-4-2011')
+
+insert into employee_payroll (Name, Salary, StartDate) values ('Sunny', '5000', '25-4-2021')
+
+insert into employee_payroll (Name, Salary, StartDate) values ('BunnySunny', '15000', '12-3-2020')
+
+insert into employee_payroll (Name, Salary, StartDate) values ('Bunty', '100', '15-6-2019')
+
+
