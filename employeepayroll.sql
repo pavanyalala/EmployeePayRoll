45a675276ffb221bb7be1d3087bd5a0161fcf645
pavanyalala <pavanyalala4508@gmail.com>
2021-08-26 14:54:07 +0530

    [pavan]:UC2:mployeepayroll Table added

--- employeepayroll.sql
+++ employeepayroll.sql
@@ -1,1 +1,9 @@
-create database EmployeePayRoll
+  
+create database EmployeePayRoll  
+
+Create Table employee_payroll (
+Id int identity( 1,1) primary key,
+Name varchar(20),
+Salary Varchar(10),
+StartDate varchar(10) 
+)
