1db2209f37d59225e2a792334cb25b9d57d41ea1
pavanyalala <pavanyalala4508@gmail.com>
2021-08-27 20:26:42 +0530

    [pavan]:UC6-Added Gender Column in data base

--- employeepayroll.sql
+++ employeepayroll.sql
@@ -16,10 +16,25 @@ insert into employee_payroll (Name, Salary, StartDate) values ('BunnySunny', '15
 
 insert into employee_payroll (Name, Salary, StartDate) values ('Bunty', '100', '15-6-2019')
 
+insert into employee_payroll (Name, Salary, StartDate) values ('Honey', '1000', '15-6-2018')
+
 select * from employee_payroll
 
 Select Salary from employee_payroll where Name='bunny'
 
 Select Salary from employee_payroll where Startdate between ('1-1-2018') and ('24-8-2021')
+
+ alter table employee_payroll ADD Gender varchar(2)
+
+  Update employee_payroll
+
+  set Gender='M' where name = 'Bunny' or name = 'Sunny' or name = 'BunnySunny' or name = 'Bunty'  
+
+   Update employee_payroll
+
+  set Gender='F' where name = 'Honey'
+
+ 
+
 
 
